58fb9bcaa89ee66cf0720b8916faccebb1f546c6
avinashiyer <avinashiyer4292@gmail.com>
2018-08-20 18:31:04 -0700

    Added map implementation

--- utility-functions-implementation/array-methods.js
+++ utility-functions-implementation/array-methods.js
@@ -7,9 +7,25 @@ Array.prototype.myForEach = function(callback){
     }
 }
 
-let arr = [1,2,3,4,5];
-arr.myForEach( (curr, index, arr) =>{
-    console.log(`Hey this is ${curr}`);
-});
+// let arr = [1,2,3,4,5];
+// arr.myForEach( (curr, index, arr) =>{
+//     console.log(`Hey this is ${curr}`);
+// });
 
-console.log(`arr is: ${arr}`);
+
+// **************************** Map implementation *******************************
+Array.prototype.myMap = function(callback){
+    let result=[];
+    for(let i=0;i<this.length;i++){
+        result.push(callback(this[i], i , this));
+        
+    }
+    return result;
+}
+
+let arr = [1,2,3,4,5];
+let arr2 = arr.myMap( (curr,index, arr) => {
+    return curr*10;
+})
+
+console.log(`Arr2 : ${arr2}`);
